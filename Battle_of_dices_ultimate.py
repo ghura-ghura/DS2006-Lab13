e7e709dc04d6e15a387f767b64c49ba76a017e68
ghura-ghura <133047641+ghura-ghura@users.noreply.github.com>
2025-10-14 10:06:47 +0200

    Added the working file that I have
    So we can continue to build on it

---
+++ Battle_of_dices_ultimate.py
@@ -0,0 +1,115 @@
+import random 
+import sys
+import copy
+import pandas as pd 
+
+
+# Dice class for the number of sides of the die and the roll mapping that we had previously
+class Die():
+    def __init__(self, sides:int):
+        if sides < 4 or sides >100:
+            raise ValueError("Die must as we asked for have a value between 4 .. 100. Please try again!!! ")
+        self.sides = sides 
+    
+    # Rolling the die 
+    def roll_die(self):
+         return random.randint(1,self.sides)
+    
+    # returning the die name that we short circuted with our new verison by just adding sides directly instead of input d1 .. dn    
+    def __str__(self):
+         return (f"D{self.sides}")
+
+
+# Define class for Players and their rolling/wins 
+class Player():
+    def __init__(self, name: str, email: str, country: str, die1: Die, die2: Die):
+        self.name = name
+        self.age = email
+        self.country = country
+        self.die1 = die1
+        self.die2 = die2
+        self.wins = 0 
+        self.rollsintotal = []
+
+    # Roll the two dices 
+    def make_roll(self):
+        roll_1 = self.die1.roll_die()
+        roll_2 = self.die2.roll_die()
+        total = roll_1 + roll_2
+        self.rollsintotal.append(total)
+        print(f"{self.name} rolled a {roll_1} and {roll_2} which in total equals {total}")
+        return total
+    
+    # Players wins
+    def add_win(self):
+        self.wins += 1 
+
+
+    
+# Game controller that runs the game 
+class Gamecontroller():
+    def __init__(self, players, winning_score:3):
+        self.players = players
+        self.winning_score = winning_score
+        self.rounds_played = 0 
+        self.over = False
+    
+    # Play the game 
+    def Play_game(self):
+        # Run until we have a winner of our 3 rounds 
+        while self.over is not True:
+            self.rounds_played += 1 
+            print(f"Rounds played for this game {self.rounds_played} ")
+
+            curr_rolls = {}
+            for i in self.players: 
+                roll = player.make_roll()
+                curr_rolls[player.name] = roll
+            
+
+
+
+
+    #Save the results to the file 
+
+
+
+"""
+class Scoreboard():
+    def __init__(self):
+        # We need to incert here 
+
+class Diemapping():
+    def __init__(self):
+        # We need to incert here 
+"""
+
+## 1. We need to input the number of players that we have along with their info 
+
+# List to store the object info for each player: 
+players = []
+
+# The input for how many plyers that we should have 
+number_of_players = int(input("Enter how many players that you want to play this game! "))
+
+######### POSSIBILITY TO CHECK INPUT? ########
+
+# Loop over the input so that we can store the objects and get the dies for each player 
+for i in range(number_of_players):
+    name = input(str(f"What is the name of Player {i+1}? "))
+    email = input(str(f"What is the email of Player {i+1}? "))
+    country = input(str(f"What is the country of origin of Player {i+1}? "))
+    die_1 = input(f"Please enter the first die that the player {name} should roll with? \n You can chose from the selection of 4 .. 100 sides? ")
+    die_2 = input(f"Please enter the first die that the player {name} should roll with? \n You can chose from the selection of 4 .. 100 sides? ")
+    player = Player(name, email, country, die_1, die_2)
+    players.append(player)
+
+## 2. We need to map the die to what die it actually corresponds and 
+
+
+## 3. We need to utilize the gamecontroller and run the game 
+
+
+## 4. Once we have a winner we need to store the information to a seperate file 
+
+
